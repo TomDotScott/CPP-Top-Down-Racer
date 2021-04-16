492b468418b365227cceca334dc676411a0d620c
TomDotScott <tom.scott45456@gmail.com>
2021-04-16 12:21:26 +0100

    Communication between client and server
    A simple client-server connection was set up, and data was sent along the TCP socket

--- NMG ICA/NMG ICA.cpp
+++ NMG ICA/NMG ICA.cpp
@@ -1,140 +1,240 @@
 #include <cmath>
+#include <iostream>
 #include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
 
-using namespace sf;
+//const int num = 8; //checkpoints
+//// TODO: use checkpoint to make sure we are on the track.
+//// Slow speed when not on the track.
+//int points[num][2] = { 300, 610,
+//  1270,430,
+//  1380,2380,
+//  1900,2460,
+//  1970,1700,
+//  2550,1680,
+//  2560,3150,
+//  500, 3300 };
+//
+//struct Car
+//{
+//    float x, y, speed, angle; int n;
+//    Car() { speed = 2; angle = 0; n = 0; }
+//    void move()
+//    {
+//        x += sin(angle) * speed;
+//        y -= cos(angle) * speed;
+//    }
+//    void findTarget()
+//    {
+//        float tx = points[n][0];
+//        float ty = points[n][1];
+//        float beta = angle - atan2(tx - x, -ty + y);
+//        if (sin(beta) < 0) angle += 0.005 * speed; else angle -= 0.005 * speed;
+//        // Check if passed a checkpoint
+//        if ((x - tx) * (x - tx) + (y - ty) * (y - ty) < 25 * 25) n = (n + 1) % num; // TODO: simplify
+//    }
+//};
 
-const int num = 8; //checkpoints
-// TODO: use checkpoint to make sure we are on the track.
-// Slow speed when not on the track.
-int points[num][2] = { 300, 610,
-  1270,430,
-  1380,2380,
-  1900,2460,
-  1970,1700,
-  2550,1680,
-  2560,3150,
-  500, 3300 };
 
-struct Car
+void runTcpServer(unsigned short port)
 {
-    float x, y, speed, angle; int n;
-    Car() { speed = 2; angle = 0; n = 0; }
-    void move()
-    {
-        x += sin(angle) * speed;
-        y -= cos(angle) * speed;
-    }
-    void findTarget()
-    {
-        float tx = points[n][0];
-        float ty = points[n][1];
-        float beta = angle - atan2(tx - x, -ty + y);
-        if (sin(beta) < 0) angle += 0.005 * speed; else angle -= 0.005 * speed;
-        // Check if passed a checkpoint
-        if ((x - tx) * (x - tx) + (y - ty) * (y - ty) < 25 * 25) n = (n + 1) % num; // TODO: simplify
-    }
-};
+	// Create a server socket to accept new connections
+	sf::TcpListener listener;
+
+	// Listen to the given port for incoming connections
+	if (listener.listen(port) != sf::Socket::Done)
+		return;
+	std::cout << "Server is listening to port " << port << ", waiting for connections... " << std::endl;
+
+	// Wait for a connection
+	sf::TcpSocket socket;
+	if (listener.accept(socket) != sf::Socket::Done)
+		return;
+	std::cout << "Client connected: " << socket.getRemoteAddress() << std::endl;
+
+	// Send a message to the connected client
+	const char out[] = "Hi, I'm the server";
+	if (socket.send(out, sizeof(out)) != sf::Socket::Done)
+		return;
+	std::cout << "Message sent to the client: \"" << out << "\"" << std::endl;
+
+	// Receive a message back from the client
+	char in[128];
+	std::size_t received;
+	if (socket.receive(in, sizeof(in), received) != sf::Socket::Done)
+		return;
+	std::cout << "Answer received from the client: \"" << in << "\"" << std::endl;
+}
+
+
+
+void runTcpClient(unsigned short port)
+{
+	// Ask for the server address
+	sf::IpAddress server = sf::IpAddress::getLocalAddress();
+	
+	// Create a socket for communicating with the server
+	sf::TcpSocket socket;
+
+	// Connect to the server
+	if (socket.connect(server, port) != sf::Socket::Done)
+		return;
+	std::cout << "Connected to server " << server << std::endl;
+
+	// Receive a message from the server
+	char in[128];
+	std::size_t received;
+	if (socket.receive(in, sizeof(in), received) != sf::Socket::Done)
+		return;
+	std::cout << "Message received from the server: \"" << in << "\"" << std::endl;
+
+	// Send an answer to the server
+	const char out[] = "Hi, I'm a client";
+	if (socket.send(out, sizeof(out)) != sf::Socket::Done)
+		return;
+	std::cout << "Message sent to the server: \"" << out << "\"" << std::endl;
+}
 
 int main()
 {
-    RenderWindow app(VideoMode(640, 480), "Car Racing Game!");
-    app.setFramerateLimit(60);
-    Texture t1, t2;
-    t1.loadFromFile("images/background.png");
-    t2.loadFromFile("images/car.png");
-    t1.setSmooth(true);
-    t2.setSmooth(true);
-    Sprite sBackground(t1), sCar(t2);
-    sBackground.scale(2, 2);
-    sCar.setOrigin(22, 22);
-    float R = 22;
-    const int N = 5;
-    Car car[N];
+	char cs{};
 
-    // Starting positions
-    for (int i = 0; i < N; i++)
-    {
-        car[i].x = 300 + i * 50;
-        car[i].y = 1700 + i * 80;
-        car[i].speed = 7 + i;
-    }
-    float speed = 0, angle = 0;
-    float maxSpeed = 12.0;
-    float acc = 0.2, dec = 0.3;
-    float turnSpeed = 0.08;
-    int offsetX = 0, offsetY = 0;
-    while (app.isOpen())
-    {
-        Event e;
-        while (app.pollEvent(e))
-        {
-            if (e.type == Event::Closed)
-                app.close();
-        }
-        bool Up = 0, Right = 0, Down = 0, Left = 0;
-        if (Keyboard::isKeyPressed(Keyboard::Up)) Up = 1;
-        if (Keyboard::isKeyPressed(Keyboard::Right)) Right = 1;
-        if (Keyboard::isKeyPressed(Keyboard::Down)) Down = 1;
-        if (Keyboard::isKeyPressed(Keyboard::Left)) Left = 1;
-        //car movement
-        if (Up && speed < maxSpeed)
-        {
-            if (speed < 0)  speed += dec;
-            else  speed += acc;
-        }
-        if (Down && speed > -maxSpeed)
-        {
-            if (speed > 0) speed -= dec;
-            else  speed -= acc;
-        }
-        if (!Up && !Down)
-        {
-            if (speed - dec > 0) speed -= dec;
-            else if (speed + dec < 0) speed += dec;
-            else speed = 0;
-        }
-        if (Right && speed != 0)  angle += turnSpeed * speed / maxSpeed;
-        if (Left && speed != 0)   angle -= turnSpeed * speed / maxSpeed;
-        car[0].speed = speed;
-        car[0].angle = angle;
-        for (int i = 0; i < N; i++) car[i].move();
-        for (int i = 1; i < N; i++) car[i].findTarget();
-        //collision
-        for (int i = 0; i < N; i++)
-            for (int j = 0; j < N; j++)
-            {
-                if (i == j)
-                {
-                    break;
-                }
-                int dx = 0, dy = 0;
-                while (dx * dx + dy * dy < 4 * R * R)
-                {
-                    car[i].x += dx / 10.0;
-                    car[i].x += dy / 10.0;
-                    car[j].x -= dx / 10.0;
-                    car[j].y -= dy / 10.0;
-                    dx = car[i].x - car[j].x;
-                    dy = car[i].y - car[j].y;
-                    if (!dx && !dy) break;
-                }
-            }
-        app.clear(Color::White);
-        // TODO: Stay within the limit of the map.
-        // TODO: Don't show white at bottom/right.
-        if (car[0].x > 320) offsetX = car[0].x - 320;
-        if (car[0].y > 240) offsetY = car[0].y - 240;
-        sBackground.setPosition(-offsetX, -offsetY);
-        app.draw(sBackground);
-        Color colors[10] = { Color::Red, Color::Green, Color::Magenta, Color::Blue, Color::White };
-        for (int i = 0; i < N; i++)
-        {
-            sCar.setPosition(car[i].x - offsetX, car[i].y - offsetY);
-            sCar.setRotation(car[i].angle * 180 / 3.141593);
-            sCar.setColor(colors[i]);
-            app.draw(sCar);
-        }
-        app.display();
-    }
-    return 0;
+	std::cin >> cs;
+
+	if (cs == 's')
+	{
+		runTcpServer(25565);
+	} else
+	{
+		runTcpClient(25565);
+	}
+
+	while (true)
+	{
+		
+	}
+	
+	//sf::RenderWindow window(sf::VideoMode(800, 600), "Server/Client Test");
+	//
+	//// run the program as long as the window is open
+	//while (window.isOpen())
+	//{
+	//	// check all the window's events that were triggered since the last iteration of the loop
+	//	sf::Event ev;
+	//	while (window.pollEvent(ev))
+	//	{
+	//		// "close requested" event: we close the window
+	//		if (ev.type == sf::Event::Closed)
+	//			window.close();
+
+	//	}
+
+	//	runTcpServer()
+	//	
+
+	//	window.clear();
+	//	
+	//	window.display();
+	//}
+	
+	//RenderWindow app(VideoMode(640, 480), "Car Racing Game!");
+	//app.setFramerateLimit(60);
+	//Texture t1, t2;
+	//t1.loadFromFile("images/background.png");
+	//t2.loadFromFile("images/car.png");
+	//t1.setSmooth(true);
+	//t2.setSmooth(true);
+	//Sprite sBackground(t1), sCar(t2);
+	//sBackground.scale(2, 2);
+	//sCar.setOrigin(22, 22);
+	//float R = 22;
+	//const int N = 5;
+	//Car car[N];
+
+	//// Starting positions
+	//for (int i = 0; i < N; i++)
+	//{
+	//    car[i].x = 300 + i * 50;
+	//    car[i].y = 1700 + i * 80;
+	//    car[i].speed = 7 + i;
+	//}
+	//float speed = 0, angle = 0;
+	//float maxSpeed = 12.0;
+	//float acc = 0.2, dec = 0.3;
+	//float turnSpeed = 0.08;
+	//int offsetX = 0, offsetY = 0;
+	//while (app.isOpen())
+	//{
+	//    Event e;
+	//    while (app.pollEvent(e))
+	//    {
+	//        if (e.type == Event::Closed)
+	//            app.close();
+	//    }
+	//    bool Up = 0, Right = 0, Down = 0, Left = 0;
+	//    if (Keyboard::isKeyPressed(Keyboard::Up)) Up = 1;
+	//    if (Keyboard::isKeyPressed(Keyboard::Right)) Right = 1;
+	//    if (Keyboard::isKeyPressed(Keyboard::Down)) Down = 1;
+	//    if (Keyboard::isKeyPressed(Keyboard::Left)) Left = 1;
+	//    //car movement
+	//    if (Up && speed < maxSpeed)
+	//    {
+	//        if (speed < 0)  speed += dec;
+	//        else  speed += acc;
+	//    }
+	//    if (Down && speed > -maxSpeed)
+	//    {
+	//        if (speed > 0) speed -= dec;
+	//        else  speed -= acc;
+	//    }
+	//    if (!Up && !Down)
+	//    {
+	//        if (speed - dec > 0) speed -= dec;
+	//        else if (speed + dec < 0) speed += dec;
+	//        else speed = 0;
+	//    }
+	//    if (Right && speed != 0)  angle += turnSpeed * speed / maxSpeed;
+	//    if (Left && speed != 0)   angle -= turnSpeed * speed / maxSpeed;
+	//    car[0].speed = speed;
+	//    car[0].angle = angle;
+	//    for (int i = 0; i < N; i++) car[i].move();
+	//    for (int i = 1; i < N; i++) car[i].findTarget();
+	//    //collision
+	//    for (int i = 0; i < N; i++)
+	//        for (int j = 0; j < N; j++)
+	//        {
+	//            if (i == j)
+	//            {
+	//                break;
+	//            }
+	//            int dx = 0, dy = 0;
+	//            while (dx * dx + dy * dy < 4 * R * R)
+	//            {
+	//                car[i].x += dx / 10.0;
+	//                car[i].x += dy / 10.0;
+	//                car[j].x -= dx / 10.0;
+	//                car[j].y -= dy / 10.0;
+	//                dx = car[i].x - car[j].x;
+	//                dy = car[i].y - car[j].y;
+	//                if (!dx && !dy) break;
+	//            }
+	//        }
+	//    app.clear(Color::White);
+	//    // TODO: Stay within the limit of the map.
+	//    // TODO: Don't show white at bottom/right.
+	//    if (car[0].x > 320) offsetX = car[0].x - 320;
+	//    if (car[0].y > 240) offsetY = car[0].y - 240;
+	//    sBackground.setPosition(-offsetX, -offsetY);
+	//    app.draw(sBackground);
+	//    Color colors[10] = { Color::Red, Color::Green, Color::Magenta, Color::Blue, Color::White };
+	//    for (int i = 0; i < N; i++)
+	//    {
+	//        sCar.setPosition(car[i].x - offsetX, car[i].y - offsetY);
+	//        sCar.setRotation(car[i].angle * 180 / 3.141593);
+	//        sCar.setColor(colors[i]);
+	//        app.draw(sCar);
+	//    }
+	//    app.display();
+	//}
+	//return 0;
 }
